79ba4eadedce7ce5bf853b56436371c2ca8d1dd8
Dan Tumaykin <d.tumaykin@gmail.com>
2012-01-27 11:22:06 +0700

    Added VS2010 project.
    Signed-off-by: Dan Tumaykin <d.tumaykin@gmail.com>

---
+++ LLCompilerTests/GlobalTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LLCompilerTests
+{
+    [TestClass]
+    public class GlobalTests
+    {
+        [TestMethod]
+        public void HelloWorld()
+        {
+            Console.Write("Hello world!");
+            Console.Read();
+        }
+    }
+}
