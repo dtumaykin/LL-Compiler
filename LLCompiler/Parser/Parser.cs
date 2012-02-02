366a674c96a3357ecd612a00c83084bbe37d3572
unknown <dultsev@ws04-7.intel.net.nsu.ru>
2012-02-02 12:47:16 +0700

    Moved ProcessConds to Parser

--- LLCompiler/Parser/Parser.cs
+++ LLCompiler/Parser/Parser.cs
@@ -12,7 +12,43 @@ namespace LLCompiler.Parser
         {
             IEnumerator<IToken> iterator = tokens.GetEnumerator();
             while (iterator.MoveNext())
-                yield return ProcessCurrentToken(iterator);
+                yield return ProcessConds(ProcessCurrentToken(iterator));
+        }
+
+        /// <summary>
+        /// Process func and changes appropriate S Expressions to cond expressions.
+        /// </summary>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        private static IParsedValue ProcessConds(IParsedValue func)
+        {
+            if (func.ParsedValueType != ParsedValuesTypes.PARSEDSEXPR) return func;
+
+            if ((func as ParsedSExpr).Members[0].ParsedValueType != ParsedValuesTypes.PARSEDIDENTIFIER)
+                throw new Exception("Something strange happened during cond conversion.\n");
+            else if (((func as ParsedSExpr).Members[0] as ParsedIdentifier).Name != "cond")
+            {
+                List<IParsedValue> newMembers = new List<IParsedValue>();
+                foreach (var i in (func as ParsedSExpr).Members)
+                {
+                    newMembers.Add(ProcessConds(i));
+                }
+                (func as ParsedSExpr).Members = newMembers;
+                return func;
+            }
+            else
+            {
+                ParsedCondExpression pce = new ParsedCondExpression();
+                var mem = (func as ParsedSExpr).Members;
+                for (int i = 1; i < mem.Count; i++)
+                {
+                    if (mem[i].ParsedValueType != ParsedValuesTypes.PARSEDSEXPR) throw new Exception("Incorrect cond clause.");
+                    var clause = mem[i] as ParsedSExpr;
+                    if (clause.Members.Count != 2) throw new Exception("Incorrect cond clause.");
+                    pce.Clauses.Add(new CondClause { Condition = ProcessConds(clause.Members[0]), Result = ProcessConds(clause.Members[1]) });
+                }
+                return pce;
+            }
         }
 
         private static IParsedValue ProcessCurrentToken(IEnumerator<IToken> iterator)
--- LLCompiler/SemanticAnalyzer/SemanticAnalyzer.cs
+++ LLCompiler/SemanticAnalyzer/SemanticAnalyzer.cs
@@ -65,7 +65,7 @@ namespace LLCompiler.SemanticAnalyzer
                 newFunc.Arguments = arguments;
 
                 // adding a body
-                newFunc.Body = ProcessConds(pse.Members[3]);
+                newFunc.Body = pse.Members[3];
 
                 // add to symbol table
                 FuncTable[newFunc.Name] = newFunc;
@@ -85,41 +85,7 @@ namespace LLCompiler.SemanticAnalyzer
                 ValidateFuncCall(func.Body);
             }
         }
-        /// <summary>
-        /// Process func and changes appropriate S Expressions to cond expressions.
-        /// </summary>
-        /// <param name="func"></param>
-        /// <returns></returns>
-        private IParsedValue ProcessConds(IParsedValue func)
-        {
-            if (func.ParsedValueType != ParsedValuesTypes.PARSEDSEXPR) return func;
-
-            if ((func as ParsedSExpr).Members[0].ParsedValueType != ParsedValuesTypes.PARSEDIDENTIFIER)
-                throw new Exception("Something strange happened during cond conversion.\n");
-            else if (((func as ParsedSExpr).Members[0] as ParsedIdentifier).Name != "cond")
-            {
-                List<IParsedValue> newMembers = new List<IParsedValue>();
-                foreach (var i in (func as ParsedSExpr).Members)
-                {
-                    newMembers.Add(ProcessConds(i));
-                }
-                (func as ParsedSExpr).Members = newMembers;
-                return func;
-            }
-            else
-            {
-                ParsedCondExpression pce = new ParsedCondExpression();
-                var mem = (func as ParsedSExpr).Members;
-                for (int i = 1; i < mem.Count; i++)
-                {
-                    if (mem[i].ParsedValueType != ParsedValuesTypes.PARSEDSEXPR) throw new Exception("Incorrect cond clause.");
-                    var clause = mem[i] as ParsedSExpr;
-                    if (clause.Members.Count != 2) throw new Exception("Incorrect cond clause.");
-                    pce.Clauses.Add(new CondClause { Condition = ProcessConds(clause.Members[0]), Result = ProcessConds(clause.Members[1]) });
-                }
-                return pce;
-            }
-        }
+        
         /// <summary>
         /// Updates function return types in base of it's arguments
         /// </summary>
